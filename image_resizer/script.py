a84d01303116a51a97aa37ddbb8e396a338962ae
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-05-22 17:49:33 +0530

    PEP8 fixes.

--- image_resizer/script.py
+++ image_resizer/script.py
@@ -1,4 +1,5 @@
-import os, sys
+import os
+import sys
 from PIL import Image
 import argparse
 
@@ -8,10 +9,10 @@ def console_main():
                 description='Downloads images from given URL')
     parser.add_argument('img2resize', nargs=1, help="Image to resize")
     parser.add_argument('-s', '--size', type=int, nargs=2, default="1024, 1024",
-                help="Size of the image output. <height>, <width>")
+                        help="Size of the image output. <height>, <width>")
     args = parser.parse_args()
     size = tuple(args.size)
-    img2resize=args.img2resize
+    img2resize = args.img2resize
     for infile in img2resize:
         outfile = os.path.splitext(infile)[0] + "_thumbnail.jpg"
         if infile != outfile:
