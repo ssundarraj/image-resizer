0cad45a23de0e3df55b599b55cb7e7ff47b9cf55
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-05-22 15:25:58 +0530

    Proper renaming added.

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
 import os, sys
 import Image
 
-size = 128, 128
+size = 1024, 1024
 
 for infile in sys.argv[1:]:
-    outfile = os.path.splitext(infile)[0] + ".thumbnail"
+    outfile = os.path.splitext(infile)[0] + "_thumbnail.jpg"
     if infile != outfile:
         try:
             im = Image.open(infile)
