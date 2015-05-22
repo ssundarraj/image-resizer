7fb73e4c9ea3b35e21b4f38194ce1e40b3b4790d
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-05-22 17:38:28 +0530

    Added size argument.

--- image_resizer/script.py
+++ image_resizer/script.py
@@ -1,10 +1,18 @@
 import os, sys
 from PIL import Image
+import argparse
+
 
 def console_main():
-    size = 1024, 1024
-
-    for infile in sys.argv[1:]:
+    parser = argparse.ArgumentParser(
+                description='Downloads images from given URL')
+    parser.add_argument('img2resize', nargs=1, help="Image to resize")
+    parser.add_argument('-s', '--size', type=int, nargs=2, default="1024, 1024",
+                help="Size of the image output. <height>, <width>")
+    args = parser.parse_args()
+    size = tuple(args.size)
+    img2resize=args.img2resize
+    for infile in img2resize:
         outfile = os.path.splitext(infile)[0] + "_thumbnail.jpg"
         if infile != outfile:
             try:
