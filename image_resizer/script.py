071871d157b4306beb879015c43a0b3131dfebf6
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-05-28 15:18:53 +0530

    Added argument for custom renaming the file.

--- image_resizer/script.py
+++ image_resizer/script.py
@@ -10,15 +10,19 @@ def console_main():
     parser.add_argument('img2resize', nargs='*', help="Image to resize")
     parser.add_argument('-s', '--size', type=int, nargs=2, default=[1024, 1024],
                         help="Size of the image output. <height>, <width>")
+    parser.add_argument('-a', '--appstring', type=str, nargs=1, default="_thumbnail",
+                        help="String to be appended to the file output.")
     args = parser.parse_args()
     size = tuple(args.size)
+    appstring = args.appstring
     img2resize = args.img2resize
     for infile in img2resize:
-        outfile = os.path.splitext(infile)[0] + "_thumbnail.jpg"
+        outfile = os.path.splitext(infile)[0] + appstring + ".jpg"
         if infile != outfile:
             try:
                 im = Image.open(infile)
                 im.thumbnail(size, Image.ANTIALIAS)
                 im.save(outfile, "JPEG")
+                print "Resied '{0}' to '{1}'.".format(infile, outfile)
             except IOError:
-                print "cannot create thumbnail for '{0}'".format(infile)
+                print "cannot create thumbnail for '{0}'.".format(infile)
